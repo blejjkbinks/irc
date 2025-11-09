7833bc1a33c8a7a86b2bb36da5780de1bb44fae3
blejj k binks <romitdomange@gmail.com>
2025-11-09 19:15:33 +0100

    :)

--- main.cpp
+++ main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
-	std::cout << "hi" << std::endl;
+	if (argc != 3)
+	{
+		std::cerr << "usage: " << argv[0] << " <port> <password>" << std::endl;
+		return (1);
+	}
+	int	port = std::atoi(argv[1]);
+	std::string	password(argv[2]);
+
 	return (0);
 }
